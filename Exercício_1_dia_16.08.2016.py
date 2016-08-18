7a4651110dbf4ce78cebc6c195ecbcb131f407e4
FelipeSantosMacedo <felipeconsultoriainformatica@gmail.com>
2016-08-17 21:56:42 -0300

    Add files via upload

---
+++ Exercício_1_dia_16.08.2016.py
@@ -0,0 +1,20 @@
+print("Alô Mundo.")
+
+texto='Eu tenho um sonho. O sonho de ver meus filhos julgados por sua personalidade, não pela cor de sua pele.'
+print(texto.upper())
+
+texto='Eu tenho um sonho. O sonho de ver meus filhos julgados por sua personalidade, não pela cor de sua pele.'
+print(texto.lower())
+
+texto='Eu tenho um sonho. O sonho de ver meus filhos julgados por sua personalidade, não pela cor de sua pele.'
+print(texto.title())
+
+print(200 + 250)
+
+print(200 / 8)
+
+print(200 * 7)
+
+print(800 - 10)
+
+print(20**2)
